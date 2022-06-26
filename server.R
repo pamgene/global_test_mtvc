73b6184f188f6437c9c1efe929e01c312d889949
rdewijn <rdwijn@pamgene.com>
2022-06-26 10:21:53 +0000

    initial

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(tercen)
 library(dplyr)
-library(tidyr)
+library(globaltest)
 
 ############################################
 #### This part should not be modified
@@ -18,36 +18,109 @@ getCtx <- function(session) {
 ####
 ############################################
 
-shinyServer(function(input, output, session) {
+server = shinyServer(function(input, output, session) {
   
   dataInput <- reactive({
-    getValues(session)
+    getDataFrame(session)
   })
   
-  output$reacOut <- renderUI({
-    plotOutput(
-      "main.plot",
-      height = input$plotHeight,
-      width = input$plotWidth
-    )
-  }) 
-  
-  output$main.plot <- renderPlot({
-    values <- dataInput()
-    data <- values$data$.y
-    hist(data)
+  cFactors = reactive({
+    c("<None>", getColumnFactors(session))
   })
   
+  observe({
+    
+    df = dataInput()
+    updateSelectInput(session, "gv", choices = levels(df$response))
+    updateSelectInput(session, "sf", choices = cFactors())
+    
+    setData = reactive({
+      ds = dataInput() %>%
+        mutate(response = relevel(response, ref = input$gv))
+      if(input$sf == "<None>"){
+        ds = data.frame(ds, Stratification.Factor = "<None>")
+      } else {
+        s = ds %>%
+          select(Stratification.Factor = input$sf)
+        ds = ds %>%
+          bind_cols(s)
+      }
+      ds
+    })
+    
+    output$design = renderTable({
+      if (input$sf == "") return()
+      aTab = setData() %>%
+        group_by(Stratification.Factor) %>%
+        dplyr::summarise(Test.Design = response %>%
+                           droplevels() %>%
+                           levels() %>%
+                           paste(collapse = "/")
+        )
+    })
+    
+    observeEvent(input$go ,{
+      result = setData() %>%
+        group_by(Stratification.Factor) %>% 
+        do(runMTvC(., input$standardize, input$directional))
+        browser()
+    })
+  })
 })
 
-getValues <- function(session){
-  ctx <- getCtx(session)
-  values <- list()
-  
-  values$data <- ctx %>% select(.y, .ri, .ci) %>%
-    group_by(.ci, .ri) %>%
-    summarise(.y = mean(.y)) # take the mean of multiple values per cell
-  
-  return(values)
+getColumnFactors = function(session){
+  ctx = getCtx(session)
+  ctx$cnames %>%
+    names()
 }
 
+getDataFrame = function(session){
+  ctx <- getCtx(session)
+  if(!ctx$hasXAxis) stop("Define variables using an x-axis in Tercen")
+  if(length(ctx$colors) != 1) stop("Define grouping / response using a single variable as color in Tercen")
+  if(length(ctx$colors) != 1) stop("Define observations using a single variable as label in Tercen")
+  df = ctx %>% 
+    select( .ri,.ci, .x, .y)
+  df = df %>% 
+    bind_cols(ctx$select(ctx$colors))
+  colnames(df) = c(colnames(df)[1:4], "response")
+  columns = ctx$cselect() %>%
+    mutate(.ci = 0:(n()-1))
+  df = df %>%
+    mutate(response = response %>% as.factor) %>%
+    left_join(columns, by = ".ci")
+  
+  df$obs = (ctx$select(ctx$labels)) %>%
+    as.matrix() %>%
+    apply(1, paste, collapse = "-")
+  
+  return(df)
+}
+
+runMTvC = function(df, standardize  = FALSE, directional = FALSE){
+  control.df = df %>%
+    filter(response == levels(response)[1])
+  df %>%
+    filter(response != levels(response)[1]) %>%
+    group_by(.ri, .ci) %>%
+    do(gtest(., control.df, standardize, directional)) %>%
+    ungroup()
+}
+
+gtest = function(df.test, df.control, standardize = FALSE, directional = FALSE){
+  df = bind_rows(df.test, df.control)
+  grp.info = df %>%
+    mutate(response = droplevels(response)) %>%
+    distinct(obs, response)
+  aGt = df %>%
+    reshape2::dcast(obs ~ .x, value.var = ".y") %>%
+    left_join(grp.info, by = "obs") %>%
+    select(-obs) %>%
+    gt(response ~ . , data = ., model = "logistic", standardize = standardize, directional = directional)
+  ddf = df %>%
+    group_by(response) %>%
+    summarize(grpm = mean(.y))
+  delta = ddf$grpm %>%
+    diff()
+  result = data.frame(p = p.value(aGt), globtest.statistic = aGt@result[2], nVariables = aGt@result[5], delta) 
+}
--- ui.R
+++ ui.R
@@ -1,20 +1,24 @@
 library(shiny)
 library(shinyjs)
 
-shinyUI(fluidPage(
+ui = shinyUI(fluidPage(
   shinyjs::useShinyjs(),
   tags$script(HTML('setInterval(function(){ $("#hiddenButton").click(); }, 1000*30);')),
   tags$footer(shinyjs::hidden(actionButton(inputId = "hiddenButton", label = "hidden"))),
   
-  titlePanel("Histogram"),
+  titlePanel("Global Test MTvC"),
   
   sidebarPanel(
-    sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-    sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
-  ),
+      selectInput("gv", "Set the reference value",choices = ""),
+      selectInput("sf", "Set a stratification factor", choices = ""),
+      checkboxInput("standardize", "Standardize", value = FALSE),
+      checkboxInput("directional", "Directional", value = FALSE),
+      actionButton("go", "Run test")
+      
+   ),
   
   mainPanel(
-    uiOutput("reacOut")
+    tableOutput("design")
   )
   
 ))
